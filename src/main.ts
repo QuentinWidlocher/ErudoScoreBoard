123aae6a45f40f643a14770c7214476555c64caf
Quentin Widlocher <qwidlocher@gmail.com>
2019-11-30 14:31:39 +0100

    :heavy_plus_sign: Add Feather Icons

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
+import feather from "feather-icons";
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+feather.replace();
