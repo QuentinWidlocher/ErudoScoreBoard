78fac4e011fbe66d1720a631dc217329a93a3e55
Quentin Widlocher <qwidlocher@gmail.com>
2019-12-02 22:34:37 +0100

    :lipstick: Fixed Card Display

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,11 +1,11 @@
 <template>
     <article class="card content">
-        <div class="card-content">
-            <div class="is-flex mb-3" style="align-items: center">
+        <div class="card-content is-flex" style="flex-direction: column; justify-items: center">
+            <div class="is-flex" style="align-items: center">
                 <h2 class="has-text-primary is-size-1 has-font-secondary mb-0 mr-3" style="word-break: keep-all">{{ score.value }}</h2>
                 <h3 class="my-0" style="word-break: break-word">{{ score.name }}</h3>
             </div>
-            <p style="word-break: break-word">{{ score.description }}</p>
+            <p v-if="score.description" class="mt-3 mb-0" style="word-break: break-word">{{ score.description }}</p>
             <span class="date">{{ score.date | date }}</span>
         </div>
     </article>
