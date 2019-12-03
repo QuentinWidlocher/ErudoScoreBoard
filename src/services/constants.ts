f1171cde52f628af8263c7fe87fcc7a8e36f5bff
Quentin Widlocher <qwidlocher@gmail.com>
2019-12-03 22:53:08 +0100

    :lipstick: Edit theme

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,7 +1,7 @@
 export default class Constants {
     public static readonly CARD_MAX_CHARS = {
         name: 50,
-        description: 60,
+        description: 90,
     }
     public static readonly CARD_REQUIRED_FIELDS = [
         'value',
